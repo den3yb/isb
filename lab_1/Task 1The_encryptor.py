2248dfb4d33ff61fd5ca6c9e41edd3f5d750957f
den3yb <den3yb@gmail.com>
2024-04-11 11:58:04 +0400

    Working with files and formatting to PEP

--- lab_1/Task 1The_encryptor.py
+++ lab_1/Task 1The_encryptor.py
@@ -1,33 +1,45 @@
 import re
 
-original_text = "Роман армянской писательницы и художницы Мериам Петросян, 'Дом, в котором...', повествует об доме-интернате для детей инвалидов с более чем столетней историей. Главный герой попал сюда совсем недавно, и с трудом приспосабливается к новому месту. В Доме полно своих тайн и странностей, необычных порядков и традиций, которые необходимо соблюдать. Например, тут не используют имён, всех без исключения, даже преподавателей и директора, зовут по кличкам таких как Сфинкс, Стервятник, Лорд, Слепой, Горбач. Дают их сами воспитанники за отличительные черты, которые, как правило, полностью описывают характер человека или его особенности."
 
-def clean_text (text: str) -> str:
-    text=re.sub('[^a-zа-яё]', '', text, flags=re.IGNORECASE)
-    text=text.lower()
+def clean_text(text: str) -> str:
+    """Удаляет все символы кроме букв у входного текста, даже пробелы, делает все буквы маленькими"""
+    text = re.sub("[^a-zа-яё]", "", text, flags=re.IGNORECASE)
+    text = text.lower()
     return text
 
-def text_to_cyphered (text: str) -> str:
-    temp = len (text)
-    while (len(text)%8!=0):
-        text =text+'а'
-    quantity = int(temp/8)
-    cyphered =''
-    for j in range(0,8):
-        for i in range(0,quantity):
-            cyphered += text[8*i+j]
-    print("Cyphered_text: ",cyphered)
+
+def text_to_cyphered(text: str) -> str:
+    """Шифрует входной текст, врзвращает зафифрованный"""
+    temp = len(text)
+    while len(text) % 8 != 0:
+        text = text + "а"
+    quantity = int(temp / 8)
+    cyphered = ""
+    for j in range(0, 8):
+        for i in range(0, quantity):
+            cyphered += text[8 * i + j]
+    print("Cyphered_text: ", cyphered)
     return cyphered
 
-def test (text: str) -> str:
-    cyphered =''
-    for j in range(0,63):
-        for i in range(0,8):
-            cyphered += text[64*i+j]
-    print("Unyphered_text: ",cyphered)
 
+def test(text: str) -> str:
+    """Осуществляет проверку-дешифровку данного конкретного текста, с учётом знания параметров таблицы"""
+    cyphered = ""
+    for j in range(0, 63):
+        for i in range(0, 8):
+            cyphered += text[64 * i + j]
+    print("Unyphered_text: ", cyphered)
+
+
+file_in = open("isb\lab_1\Task 1 Original text.txt", "r", encoding="utf-8")
+original_text = file_in.readline()
+file_in.close()
 
 cyphered_text = clean_text(original_text)
-cyphered_text =text_to_cyphered(cyphered_text)
-test(cyphered_text)
+cyphered_text = text_to_cyphered(cyphered_text)
+
+file_out = open("isb\lab_1\Task 1 Cyphered text.txt", "w", encoding="utf-8")
+file_out.write(cyphered_text)
+file_out.close()
+
 
--- lab_1/Task 2 The Decoder.py
+++ lab_1/Task 2 The Decoder.py
@@ -1,66 +1,65 @@
-original_text = "36-w0n/e\\s/e-ren\\05w2t-50n`i2\\0i21-im07e86w2`-/-2wpemq6f2ewwe1-703er6nweni2-noc0ni80wwe-23q0w2\\2ns-5e-w6k6\\6-d2me/e9e-2nre\\s3e86w2`-68ieq6i232me86ww;t-n2ni0q-e7m67ei/2-56ww;t-703er6nwenis-2wpemq6f22-5eni296\\6ns-2n/\\zk2i0\\swe-p232k0n/2q2-2-65q2w2nim6i28w;q2-q0m6q2-nre`8\\0w20q-/eqrszi0me8-ni6\\6-ek0825we1-w0e7te52qenis-2nre\\s3e86w2`-68ieq6i2k0n/2t-nm05ni8-36c2i;-p61\\e8-56ww;t-2-rme9m6qqwe1-nm05;-n\\05ozc21-xi6r-m6382i2`-68ieq6i2k0n/2t-nm05ni8-36c2i;-n8`36w-n-re`8\\0w20qm6nrm050\\0ww;t-n2ni0q-e7m67ei/2-56ww;t-2-/eqrszi0mw;t-n0i01-8-/eiem;tnm05ni86-n0i08e1-703er6nweni2-2nre\\s3ozin`-8-r0m8oz-ek0m05s-5\\`-36c2i;-r0m056860q;t-re-n0i`q-56ww;t-8-w627e\\00-re\\we1-im6/ie8/0-re5-nm05ni86q2-n0i08e1-703er6nweni2-q;-7o50q-2q0is-8-825o-q0m;-rm05ei8m6c0w2`-w6mod0w21-703er6nweni2-/eiem;0-8e3w2/6zi-rm2-r0m056k0-2wpemq6f22-ren0i`q-6-i6/40-q0m;-re38e\\`zc20-erm050\\`is-kie-i6/20-w6mod0w2`-703er6nweni2-2q0\\2-q0nie?"
-
 def creat_dict(text: str) -> dict:
+    """Создаёт словарь из входной строки,  в качестве ключа - слово, в качестве значения - частота встречи"""
     long = len(original_text)
-    dictionary={}
+    dictionary = {}
     for symbol in text:
         if symbol in dictionary:
-            dictionary[symbol] +=1/long*100
+            dictionary[symbol] += 1 / long * 100
         else:
-            dictionary[symbol] = 1/long*100
+            dictionary[symbol] = 1 / long * 100
 
     return dictionary
 
+
 def print_dictionary(dictionary: dict) -> None:
-    for key,value in dictionary.items(): print(key, ':', value)
+    """Красиво выводит входной словарь"""
+    for key, value in dictionary.items():
+        print(key, ":", value)
+
+
+file_in = open("isb\lab_1\cod9.txt", "r", encoding="utf-8")
+original_text = file_in.readline()
+file_in.close()
 
 
 dictionary_chastot = creat_dict(original_text)
 print_dictionary(dictionary_chastot)
 
-original_text = original_text.replace('-', ' ')
-
-original_text = original_text.replace('/', 'К')
-original_text = original_text.replace('2', 'И')
-original_text = original_text.replace('n', 'С')
-original_text = original_text.replace('8', 'В')
-original_text = original_text.replace('6', 'А')
-original_text = original_text.replace('s', 'Ь')
-
-original_text = original_text.replace('5', 'Д')
-original_text = original_text.replace('e', 'О')
-original_text = original_text.replace('r', 'П')
-original_text = original_text.replace('3', '3')
-
-
-
-
-original_text = original_text.replace('0', 'Е')
-original_text = original_text.replace('i', 'Т')
-original_text = original_text.replace('w', 'Н')
-original_text = original_text.replace('m', 'Р')
-original_text = original_text.replace('q', 'М')
-
-
-original_text = original_text.replace('\\', 'Л')
-
-original_text = original_text.replace(';', 'Ы')
-original_text = original_text.replace('`', 'Я')
-original_text = original_text.replace('7', 'Б')
-original_text = original_text.replace('t', 'Х')
-original_text = original_text.replace('1', 'Й')
-original_text = original_text.replace('k', 'Ч')
-original_text = original_text.replace('o', 'У')
-original_text = original_text.replace('z', 'Ю')
-original_text = original_text.replace('c', 'Щ')
-original_text = original_text.replace('p', 'Ф')
-original_text = original_text.replace('9', 'Г')
-original_text = original_text.replace('f', 'Ц')
-original_text = original_text.replace('d', 'Ш')
-original_text = original_text.replace('4', 'Ж')
-original_text = original_text.replace('?', 'Ъ')
-original_text = original_text.replace('x', 'Э')
+original_text = original_text.replace("-", " ")
+original_text = original_text.replace("/", "К")
+original_text = original_text.replace("2", "И")
+original_text = original_text.replace("n", "С")
+original_text = original_text.replace("8", "В")
+original_text = original_text.replace("6", "А")
+original_text = original_text.replace("s", "Ь")
+original_text = original_text.replace("5", "Д")
+original_text = original_text.replace("e", "О")
+original_text = original_text.replace("r", "П")
+original_text = original_text.replace("3", "3")
+original_text = original_text.replace("0", "Е")
+original_text = original_text.replace("i", "Т")
+original_text = original_text.replace("w", "Н")
+original_text = original_text.replace("m", "Р")
+original_text = original_text.replace("q", "М")
+original_text = original_text.replace("\\", "Л")
+original_text = original_text.replace(";", "Ы")
+original_text = original_text.replace("`", "Я")
+original_text = original_text.replace("7", "Б")
+original_text = original_text.replace("t", "Х")
+original_text = original_text.replace("1", "Й")
+original_text = original_text.replace("k", "Ч")
+original_text = original_text.replace("o", "У")
+original_text = original_text.replace("z", "Ю")
+original_text = original_text.replace("c", "Щ")
+original_text = original_text.replace("p", "Ф")
+original_text = original_text.replace("9", "Г")
+original_text = original_text.replace("f", "Ц")
+original_text = original_text.replace("d", "Ш")
+original_text = original_text.replace("4", "Ж")
+original_text = original_text.replace("?", "Ъ")
+original_text = original_text.replace("x", "Э")
 
 
-f=open('Task 2 Uncyphered text.txt', 'w')
-f.write(original_text)
+file_out = open("isb\lab_1\Task 2 Uncyphered text.txt", "w", encoding="utf-8")
+file_out.write(original_text)
+file_out.close()
