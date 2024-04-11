582ffb7c9a55825a11163b5b72a37a269a6258da
den3yb <den3yb@gmail.com>
2024-04-11 12:52:26 +0400

    Creat Key file for both task, Minor changes of cod

--- lab_1/Task 1The_encryptor.py
+++ lab_1/Task 1The_encryptor.py
@@ -11,24 +11,33 @@ def clean_text(text: str) -> str:
 def text_to_cyphered(text: str) -> str:
     """Шифрует входной текст, врзвращает зафифрованный"""
     temp = len(text)
-    while len(text) % 8 != 0:
+    while temp % 63 != 0:
         text = text + "а"
-    quantity = int(temp / 8)
+        temp += 1
+    quantity = int(temp / 63)
     cyphered = ""
-    for j in range(0, 8):
+    for j in range(0, 63):
         for i in range(0, quantity):
-            cyphered += text[8 * i + j]
+            cyphered += text[63 * i + j]
     print("Cyphered_text: ", cyphered)
     return cyphered
 
 
 def test(text: str) -> str:
     """Осуществляет проверку-дешифровку данного конкретного текста, с учётом знания параметров таблицы"""
-    cyphered = ""
-    for j in range(0, 63):
-        for i in range(0, 8):
-            cyphered += text[64 * i + j]
-    print("Unyphered_text: ", cyphered)
+    file_key = open("isb\lab_1\Task 1 Key.txt", "w", encoding="utf-8")
+
+    uncyphered = ""
+    for j in range(0, 8):
+        for i in range(0, 63):
+            uncyphered += text[8 * i + j]
+            file_key.write(" " + text[9 * i + j])
+        file_key.write("\n")
+    file_key.write(
+        "\n\n Записываем текст в строчку, когда заканчивается переходим на следующую, для шифровки после получение таблицы, выписывает столбцы"
+    )
+    file_key.close()
+    return uncyphered
 
 
 file_in = open("isb\lab_1\Task 1 Original text.txt", "r", encoding="utf-8")
@@ -37,6 +46,7 @@ file_in.close()
 
 cyphered_text = clean_text(original_text)
 cyphered_text = text_to_cyphered(cyphered_text)
+test(cyphered_text)
 
 file_out = open("isb\lab_1\Task 1 Cyphered text.txt", "w", encoding="utf-8")
 file_out.write(cyphered_text)
