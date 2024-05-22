eb90dd3fe5d629482a1068c12af0402afbc95845
den3yb <den3yb@gmail.com>
2024-05-22 21:50:16 +0400

    complete all tests

--- lab_2/tests/frequency_bit_test.py
+++ lab_2/tests/frequency_bit_test.py
@@ -1,8 +1,9 @@
 import os
 import json
 import math
+import mpmath
 
-from const import PATH
+from const import PATH, P
 
 def frequency_bit_test(seq: str) -> float:
     s = 0
@@ -15,7 +16,46 @@ def frequency_bit_test(seq: str) -> float:
     sn = abs(sn)
     return math.erfc(sn / math.sqrt(2))
 
+def same_bit_test(seq: str) -> float:
+    lenght = len(seq)
+    count = seq.count('1')
+    s = count/lenght
+    if (abs(s - 0.5) >= 2/math.sqrt(lenght)):
+        return 0
+    vn = 0
+    for i in range(1, lenght):
+        if seq[i-1] != seq[i]:
+            vn +=1
+    temp =abs(vn - 2*lenght*s*(1 - s)) 
+    temp = temp / (2*math.sqrt(2*lenght)*s*(1 - s))
+    return math.erfc(temp)
 
+def longest_bit_seq(seq: str) -> float:
+    length = len(seq)
+    v = [0]*4
+    for i in range (0, length, 8):
+        maximum = 0
+        temp = 0
+        for bit in seq[i : (i+8)]:
+            if bit == '1':
+                temp += 1
+                maximum = max(temp, maximum)
+            else:
+                temp = 0
+        if (maximum <= 1):
+            v[0] +=1
+        elif (maximum == 2):
+            v[1] +=1
+        elif (maximum == 3):
+            v[2] +=1
+        else:
+            v[3] +=1
+    x = 0
+    for i in range(0, 4):
+        x += (v[i] - 16*P[i])**2 / (16*P[i])
+    return mpmath.gammainc(3/2,x/2)
+
+            
 
 if __name__ == "__main__":
 
@@ -38,13 +78,27 @@ if __name__ == "__main__":
         print("Ошибка открытия файла с последовательностью java: ", e)  
 
     try:
-        cpp_res = frequency_bit_test(cpp_seq)
-        java_res = frequency_bit_test(java_seq)
+        cpp_res_freq = frequency_bit_test(cpp_seq)
+        print(cpp_res_freq)
+        java_res_freq = frequency_bit_test(java_seq)
+        print(java_res_freq)
     except Exception as e:
-        print("Ошибка расчёта теста для с++: ", e) 
+        print("Ошибка расчёта частотного побитового теста для с++: ", e) 
 
     try:
-        with open(ways[2], "w", encoding="utf-8") as  file_out:
-            file_out.write("Результаты Побитового частотного теста: \nДля генератора с++: " + str(cpp_res) + "\nДля джавы: "+ str(java_res))
+        cpp_res_same = same_bit_test(cpp_seq)
+        print(cpp_res_same)
+        java_res_same = same_bit_test(java_seq)
+        print(java_res_same)
     except Exception as e:
-        print("Ошибка при записи результатов теста", e)
+        print("Ошибка расчёта теста на одинаковые подряд идущие биты для с++: ", e) 
+
+    
+    try:
+        cpp_res_long = longest_bit_seq(cpp_seq)
+        print(cpp_res_long)
+        java_res_long = longest_bit_seq(java_seq)
+        print(java_res_long)
+    except Exception as e:
+        print("Ошибка расчёта теста на самую длинную последовательность единиц в блоке для с++: ", e) 
+
