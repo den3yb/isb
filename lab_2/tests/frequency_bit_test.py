86dba541a6327e0d6d21a93026874498cfd50fb7
den3yb <den3yb@gmail.com>
2024-06-06 13:25:18 +0400

    finish pls

--- lab_2/tests/frequency_bit_test.py
+++ lab_2/tests/frequency_bit_test.py
@@ -7,55 +7,67 @@ from const import PATH, P
 
 
 def frequency_bit_test(seq: str) -> float:
-    s = seq.count("1")
-    temp = seq.count("0")
-    s = s - temp
-    sn = s / (math.sqrt(len(seq)))
-    sn = abs(sn)
-    return math.erfc(sn / math.sqrt(2))
+    try:
+        s = seq.count("1")
+        temp = seq.count("0")
+        s = s - temp
+        sn = s / (math.sqrt(len(seq)))
+        sn = abs(sn)
+        return math.erfc(sn / math.sqrt(2))
+    except Exception as e:
+        print("Ошибка расчёта частотного побитового теста: ", e)
 
 
 def same_bit_test(seq: str) -> float:
-    lenght = len(seq)
-    count = seq.count("1")
-    s = count / lenght
-    if abs(s - 0.5) >= 2 / math.sqrt(lenght):
-        return 0
-    vn = 0
-    for i in range(1, lenght):
-        if seq[i - 1] != seq[i]:
-            vn += 1
-    temp = abs(vn - 2 * lenght * s * (1 - s))
-    temp = temp / (2 * math.sqrt(2 * lenght) * s * (1 - s))
-    return math.erfc(temp)
+    try:
+        lenght = len(seq)
+        count = seq.count("1")
+        s = count / lenght
+        if abs(s - 0.5) >= 2 / math.sqrt(lenght):
+            return 0
+        vn = 0
+        for i in range(1, lenght):
+            if seq[i - 1] != seq[i]:
+                vn += 1
+        temp = abs(vn - 2 * lenght * s * (1 - s))
+        temp = temp / (2 * math.sqrt(2 * lenght) * s * (1 - s))
+        return math.erfc(temp)
+    except Exception as e:
+        print("Ошибка расчёта теста на одинаковые подряд идущие биты: ", e)
+
 
 
 def longest_bit_seq(seq: str) -> float:
-    length = len(seq)
-    v = [0] * 4
-    for i in range(0, length, 8):
-        maximum = 0
-        temp = 0
-        for bit in seq[i : (i + 8)]:
-            if bit == "1":
-                temp += 1
-                maximum = max(temp, maximum)
-            else:
-                temp = 0
-        match maximum:
-            case _ if maximum <= 1:
-                v[0] += 1
-            case 2:
-                v[1] += 1
-            case 3:
-                v[2] += 1
-            case _:
-                v[3] += 1
-    x = 0
-    for i in range(0, 4):
-        x += (v[i] - 16 * P[i]) ** 2 / (16 * P[i])
-    return mpmath.gammainc(3 / 2, x / 2)
-
+    try:
+        length = len(seq)
+        v = [0] * 4
+        for i in range(0, length, 8):
+            maximum = 0
+            temp = 0
+            for bit in seq[i : (i + 8)]:
+                if bit == "1":
+                    temp += 1
+                    maximum = max(temp, maximum)
+                else:
+                    temp = 0
+            match maximum:
+                case _ if maximum <= 1:
+                    v[0] += 1
+                case 2:
+                    v[1] += 1
+                case 3:
+                    v[2] += 1
+                case _:
+                    v[3] += 1
+        x = 0
+        for i in range(0, 4):
+            x += (v[i] - 16 * P[i]) ** 2 / (16 * P[i])
+        return mpmath.gammainc(3 / 2, x / 2)
+    except Exception as e:
+        print(
+            "Ошибка расчёта теста на самую длинную последовательность единиц в блоке: ",
+            e,
+        )
 
 if __name__ == "__main__":
 
@@ -77,26 +89,19 @@ if __name__ == "__main__":
     except Exception as e:
         print("Ошибка открытия файла с последовательностью java: ", e)
 
-    try:
-        cpp_res_freq = frequency_bit_test(cpp_seq)
-        java_res_freq = frequency_bit_test(java_seq)
-    except Exception as e:
-        print("Ошибка расчёта частотного побитового теста: ", e)
 
-    try:
-        cpp_res_same = same_bit_test(cpp_seq)
-        java_res_same = same_bit_test(java_seq)
-    except Exception as e:
-        print("Ошибка расчёта теста на одинаковые подряд идущие биты: ", e)
+    cpp_res_freq = frequency_bit_test(cpp_seq)
+    java_res_freq = frequency_bit_test(java_seq)
+    
 
-    try:
-        cpp_res_long = longest_bit_seq(cpp_seq)
-        java_res_long = longest_bit_seq(java_seq)
-    except Exception as e:
-        print(
-            "Ошибка расчёта теста на самую длинную последовательность единиц в блоке: ",
-            e,
-        )
+
+    cpp_res_same = same_bit_test(cpp_seq)
+    java_res_same = same_bit_test(java_seq)
+    
+
+   
+    cpp_res_long = longest_bit_seq(cpp_seq)
+    java_res_long = longest_bit_seq(java_seq)
 
     try:
         with open(ways[2], "w", encoding="utf-8") as file_out:
